d77924f1b6821fe034b2e2379b34a7e2b1c1943e
ahmdfadilah7 <ahmdfadilah7@gmail.com>
2026-08-20 18:26:03 +0700

    update: seo tubevault

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,32 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
 /*
+| SEO: pastikan sitemap & robots selalu file statis, bukan SPA HTML
+| (aman jika request masuk lewat index.php tanpa rewrite file).
+*/
+Route::get('/robots.txt', function () {
+    $path = public_path('robots.txt');
+
+    abort_unless(File::isFile($path), 404);
+
+    return response(File::get($path), 200, [
+        'Content-Type' => 'text/plain; charset=UTF-8',
+        'Cache-Control' => 'public, max-age=3600',
+    ]);
+});
+
+Route::get('/sitemap.xml', function () {
+    $path = public_path('sitemap.xml');
+
+    abort_unless(File::isFile($path), 404);
+
+    return response(File::get($path), 200, [
+        'Content-Type' => 'application/xml; charset=UTF-8',
+        'Cache-Control' => 'public, max-age=3600',
+    ]);
+});
+
+/*
 | SPA: jika index.html ada (production), layani TubeVault.
 | Tanpa ini, Hostinger yang mengarahkan semua ke index.php tetap menampilkan welcome Laravel.
 */
